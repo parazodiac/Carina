bdbd1333ea40961e7bd6c663a99d66bbc8d8c9f9
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-11-07 11:08:10 -0500

    adding bufwriter

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 use std::path::{Path, PathBuf};
 
 use clap::ArgMatches;
@@ -20,4 +20,22 @@ pub fn file_path_from_clap(sub_m: &ArgMatches, clap_id: &str) -> Result<PathBuf,
 
 pub fn bufreader_from_filepath(path: PathBuf) -> Result<BufReader<File>, Box<dyn Error>> {
     Ok(BufReader::new(File::open(path)?))
+}
+
+pub fn bufwriter_from_clap(
+    sub_m: &ArgMatches,
+    clap_id: &str,
+) -> Result<BufWriter<File>, Box<dyn Error>> {
+    let file_str = sub_m
+        .value_of(clap_id)
+        .expect(&format!("can't find the flag: {}", clap_id));
+
+    let file = BufWriter::new(File::create(file_str)?);
+    let file_path = Path::new(file_str)
+        .canonicalize()
+        .expect(&format!("Can't find absolute path of file tag {}", clap_id));
+
+    info!("Created File w/ tag \"{}\" at {:?}", clap_id, file_path);
+
+    Ok(file)
 }
